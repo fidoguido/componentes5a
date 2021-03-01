55c60824d3445850024ca2f8eb8aa0988235cb7b
fidoguido <guido.alvarez.martinez@gmail.com>
2021-03-01 16:58:39 -0500

    Primer Commit

---
+++ lib/src/pages/home_temp.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class HomePageTemp extends StatelessWidget {
+  final opciones = ['Uno', 'Dos', 'Tres', 'Cuatro', 'Cinco', 'Seis'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Componentes'),
+      ),
+      body: ListView(
+        children: _crearItems(),
+      ),
+    );
+  }
+
+  List<Widget> _crearItems() {
+    List<Widget> lista = new List<Widget>();
+    for (String item in opciones) {
+      final tempWiget = ListTile(
+        title: Text(item),
+        subtitle: Text('Cualquier cosa'),
+        leading: Icon(Icons.accessibility),
+        trailing: Icon(Icons.ac_unit_rounded),
+        onTap: () {},
+      );
+      lista.add(tempWiget);
+    }
+    return lista;
+  }
+}
